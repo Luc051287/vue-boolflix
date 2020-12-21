89112f3ab5a30413a2a45c4cb51e0441c6f0864e
Luca Gentili <l.gentili87@icloud.com>
2020-12-21 16:33:03 +0100

    Fourteenth Version

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var app = new Vue(
     data: {
       search: '',
       voteClass: '',
+      genres: [],
       resultsArr: [],
       result: false,
       cast: [],
@@ -24,6 +25,7 @@ var app = new Vue(
         let promiseMovie = axios.get('https://api.themoviedb.org/3/search/movie?',{
           params: {
             api_key: '71648f6532d78651db76cf10430d87ef',
+            // language: "it_IT",
             page: '1',
             query: this.search.trim().toLowerCase()
           }
@@ -31,6 +33,7 @@ var app = new Vue(
         let promiseTV = axios.get('https://api.themoviedb.org/3/search/tv?',{
           params: {
             api_key: '71648f6532d78651db76cf10430d87ef',
+            // language: "it_IT",
             page: '1',
             query: this.search.trim().toLowerCase()
           }
@@ -53,6 +56,7 @@ var app = new Vue(
           }
         }).then((response) => {
           for (names of response.data.cast) {
+            // manca gestire l'undefined
             if (this.cast.length < 5) {
               this.cast.push(names.name);
             }
@@ -76,10 +80,34 @@ var app = new Vue(
           }
         });
         return stars;
+      },
+      findGenres: function(arrayID) {
+        return arrayID.map((e1,index) => {
+          let str = '';
+          this.genres.forEach(e2 => {
+            if (e1 == e2.id) {
+              str = e2.name;
+              return
+            }
+          })
+          return str
+        })
       }
     },
-    computed: {
-
+    mounted: function()  {
+        let promiseMovie = axios.get('https://api.themoviedb.org/3/genre/movie/list?',{
+          params: {
+            api_key: '71648f6532d78651db76cf10430d87ef',
+          }
+        });
+        let promiseTV = axios.get('https://api.themoviedb.org/3/genre/tv/list?',{
+          params: {
+            api_key: '71648f6532d78651db76cf10430d87ef',
+          }
+        });
+        Promise.all([promiseMovie, promiseTV]).then((values) => {
+          this.genres = values[0].data.genres.concat(values[1].data.genres)
+        });
     }
   }
 );
