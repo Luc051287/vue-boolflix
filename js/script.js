2ec3d166e6aa5866909f1e299dd92d7a85a1ec37
Luca Gentili <l.gentili87@icloud.com>
2021-03-05 09:33:15 +0100

    Final

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ var app = new Vue(
       tvTotPages: 0,
       genre: 'all',
       resultsArr: [],
-      title: '',
       link: 'https://image.tmdb.org/t/p/w220_and_h330_face',
       flags: {
         en: 'img/en.png',
